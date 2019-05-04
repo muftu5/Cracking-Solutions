21bb0a5ffe63bef4b15dfd377eac12cdfe8315c1
Artem Ignatovich <shelmoon@mail.ru>
2019-05-04 16:51:39 +0300

    Initial

---
+++ RemoveDups.Tests/UnitTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Common.DataStructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RemoveDups.Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var list = new SinglyLinkedList<int>();
+            list.Append(1);
+            list.Append(2);
+            list.Append(3);
+
+            list.RemoveDuplicates();
+
+            Assert.AreEqual(3, list.Enumerate().Count());
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var list = new SinglyLinkedList<int>();
+            list.Append(1);
+            list.Append(2);
+            list.Append(2);
+
+            list.RemoveDuplicates();
+
+            Assert.AreEqual(2, list.Enumerate().Count());
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var list = new SinglyLinkedList<int>();
+            list.Append(2);
+            list.Append(2);
+            list.Append(2);
+
+            list.RemoveDuplicates();
+
+            Assert.AreEqual(1, list.Enumerate().Count());
+        }
+    }
+}
---
+++ RemoveDups/Solution.cs
@@ -0,0 +1,12 @@
+﻿using Common.DataStructure;
+
+namespace RemoveDups
+{
+    public static class Solution
+    {
+        public static SinglyLinkedList<T> RemoveDups<T>(SinglyLinkedList<T> linkedList)
+        {
+
+        }
+    }
+}
