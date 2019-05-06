dfa1d268bbd4e9cac9ab04f6f502bd01260a2ac4
Artem Ignatovich <shelmoon@mail.ru>
2019-05-06 17:38:14 +0300

    PowerSet implemented

---
+++ CrackingTheCodingSolutions/PowerSet.Tests/PowerSetTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PowerSet.Tests
+{
+    [TestClass]
+    public class PowerSetTests
+    {
+        [TestMethod]
+        public void PowerSet_ShouldReturnAllSubSets()
+        {
+            var elements = new List<int>() { 1, 2, 3 };
+            var powerSet = new PowerSet(elements);
+
+            var result = powerSet.GetAllSubsets().ToList();
+            Assert.AreEqual(7, result.Count());
+        }
+    }
+}
